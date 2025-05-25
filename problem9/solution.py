8e95c5b5d0950e74f35d0fa5b0ba23a8f5a8137d
asaduzzaman-sarker <asaduzzaman.seo@gmail.com>
2025-05-25 11:51:54 +0600

    Add number 9 file

---
+++ problem9/solution.py
@@ -0,0 +1,7 @@
+# Problem-9: Find if 6 is available in the list
+my_list = [4, 8, 7, 4,3,6,2,1,9]
+number=6
+if number in my_list:
+    print ("6 is available this list")
+else:
+    print('Not found')
